227af0760a8518d8ecc981afa327fffd06d46f88
jiangqiao <miniphper@gmail.com>
2025-07-30 17:26:22 +0800

    1.4.2

---
+++ test/engines/engines.js
@@ -0,0 +1,6 @@
+
+console.log('主线程开始');
+
+Engines.executeScript("test/aaaaaaaaaa/test.js");
+console.log('主线程结束');
+
---
+++ test/engines/test.js
@@ -0,0 +1,4 @@
+
+console.log('子线程开始1');
+System.sleep(3000);
+console.log('子线程完成322112111');
